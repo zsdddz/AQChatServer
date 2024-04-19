ecb411c94d64c525935a8db2047e9f985bfc691f
zsdddz <hitd@foxmail.com>
2024-04-20 00:48:52 +0800

    feat(IM业务): 添加业务处理器
    （1）添加外层通用业务处理器

---
+++ src/main/java/com/howcode/darkchat/handler/DarkChatCommandHandler.java
@@ -0,0 +1,17 @@
+package com.howcode.darkchat.handler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @Author: ZhangWeinan
+ * @Description:
+ * @date 2024-04-20 0:35
+ */
+public class DarkChatCommandHandler extends SimpleChannelInboundHandler<Object> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object msg) throws Exception {
+
+    }
+}
