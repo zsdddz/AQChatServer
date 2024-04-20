bd2ba8736aba9142ea73450ad9efdc43990276ae
zsdddz <hitd@foxmail.com>
2024-04-20 20:34:59 +0800

    feat(IM业务): 完善编码类
    （1）完善消息编码类

--- src/main/java/com/howcode/aqchat/codec/MessageEncoder.java
+++ src/main/java/com/howcode/aqchat/codec/MessageEncoder.java
@@ -1,9 +1,15 @@
 package com.howcode.aqchat.codec;
 
 import com.google.protobuf.GeneratedMessageV3;
+import com.howcode.aqchat.message.MessageRecognizer;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,8 +22,23 @@ import java.util.List;
 @Component
 @ChannelHandler.Sharable
 public class MessageEncoder extends MessageToMessageEncoder<GeneratedMessageV3> {
-    @Override
-    protected void encode(ChannelHandlerContext channelHandlerContext, GeneratedMessageV3 msg, List<Object> list) throws Exception {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageEncoder.class);
+    @Resource
+    private MessageRecognizer recognizer;
 
+    @Override
+    protected void encode(ChannelHandlerContext ctx, GeneratedMessageV3 msg, List<Object> list) throws Exception {
+        int msgCommand = recognizer.getMsgCommandByMsgClazz(msg.getClass());
+        if (msgCommand < 0) {
+            LOGGER.error("无法识别的消息, msgClazz = {}", msg.getClass().getSimpleName());
+            return;
+        }
+        LOGGER.info("返回消息编码器: msgClazz = {}, msgCommand = {}", msg.getClass().getSimpleName(), msgCommand);
+        byte[] msgBody = msg.toByteArray();
+        ByteBuf byteBuf = ctx.alloc().buffer();
+        byteBuf.writeShort((short) msgBody.length);
+        byteBuf.writeShort((short) msgCommand);
+        byteBuf.writeBytes(msgBody);
+        list.add(new BinaryWebSocketFrame(byteBuf));
     }
 }
