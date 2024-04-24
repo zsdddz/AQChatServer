2a225b6df10fb474515e713c5104e99f28dc6ad6
zsdddz <hitd@foxmail.com>
2024-04-24 18:19:41 +0800

    feat(异常消息): 异常信息打印
    （1）添加日常信息日志

--- src/main/java/com/howcode/aqchat/message/MessageConstructor.java
+++ src/main/java/com/howcode/aqchat/message/MessageConstructor.java
@@ -1,6 +1,8 @@
 package com.howcode.aqchat.message;
 
 import com.howcode.aqchat.enums.AQChatEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author: ZhangWeinan
@@ -8,8 +10,10 @@ import com.howcode.aqchat.enums.AQChatEnum;
  * @date 2024-04-24 0:55
  */
 public class MessageConstructor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConstructor.class);
 
     public static AQChatMsgProtocol.ExceptionMsg buildExceptionMsg(AQChatEnum aqChatEnum){
+        LOGGER.error("构建异常消息: code = {}, msg = {}", aqChatEnum.getCode(), aqChatEnum.getMessage());
         return AQChatMsgProtocol.ExceptionMsg.newBuilder().setCode(aqChatEnum.getCode()).setMsg(aqChatEnum.getMessage()).build();
     }
 }
