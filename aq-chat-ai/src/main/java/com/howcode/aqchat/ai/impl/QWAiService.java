1b22ded10ee9bbe54a97ae8d26a506a064143f8a
zsdddz <hitd@foxmail.com>
2024-06-16 20:59:43 +0800

    feat(AI相关): 支持无AI使用

--- aq-chat-ai/src/main/java/com/howcode/aqchat/ai/impl/QWAiService.java
+++ aq-chat-ai/src/main/java/com/howcode/aqchat/ai/impl/QWAiService.java
@@ -8,11 +8,10 @@ import com.alibaba.dashscope.common.Role;
 import com.alibaba.dashscope.exception.ApiException;
 import com.alibaba.dashscope.exception.InputRequiredException;
 import com.alibaba.dashscope.exception.NoApiKeyException;
-import com.alibaba.dashscope.utils.JsonUtils;
 import com.howcode.aqchat.ai.AIResult;
 import com.howcode.aqchat.ai.IAiService;
+import com.howcode.aqchat.ai.config.GenerationParamHolder;
 import com.howcode.aqchat.ai.model.QWResult;
-import com.howcode.aqchat.common.config.AQChatConfig;
 import com.howcode.aqchat.common.enums.AIMessageStatusEnum;
 import io.reactivex.Flowable;
 import jakarta.annotation.Resource;
@@ -20,7 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Consumer;
 
 /**
@@ -34,7 +33,7 @@ public class QWAiService implements IAiService {
     private static final Logger LOGGER = LoggerFactory.getLogger(QWAiService.class);
 
     @Resource
-    private AQChatConfig aqChatConfig;
+    private GenerationParamHolder generationParamHolder;
 
     @Override
     public String getAnswer(String question) {
@@ -45,9 +44,9 @@ public class QWAiService implements IAiService {
     public void streamCallWithMessage(String userMsg, Consumer<AIResult> consumer) {
         Generation gen = new Generation();
         Message message = Message.builder().role(Role.USER.getValue()).content(userMsg).build();
-        try{
+        try {
             streamCallWithMessage(gen, message, consumer);
-        }catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("Error occurred while calling AI service", e);
             throw new RuntimeException(e);
         }
@@ -55,8 +54,16 @@ public class QWAiService implements IAiService {
 
     private void streamCallWithMessage(Generation gen, Message message, Consumer<AIResult> consumer)
             throws NoApiKeyException, ApiException, InputRequiredException {
-        GenerationParam param = buildGenerationParam(message);
-        Flowable<GenerationResult> result = gen.streamCall(param);
+        GenerationParam generationParam = generationParamHolder.getGenerationParam();
+        if (null == generationParam) {
+            QWResult qwResult = new QWResult();
+            qwResult.setContent("系统AI未启用，请联系管理员");
+            qwResult.setStatus(AIMessageStatusEnum.FAIL.getCode());
+            consumer.accept(qwResult);
+            return;
+        }
+        generationParam.setMessages(Collections.singletonList(message));
+        Flowable<GenerationResult> result = gen.streamCall(generationParam);
         result.blockingForEach(r -> {
             String content = r.getOutput().getChoices().get(0).getMessage().getContent();
             String finishReason = r.getOutput().getChoices().get(0).getFinishReason();
@@ -66,18 +73,6 @@ public class QWAiService implements IAiService {
             consumer.accept(qwResult);
         });
         LOGGER.info("Stream call with message completed");
-    }
-
-
-    private GenerationParam buildGenerationParam(Message userMsg) {
-        return GenerationParam.builder()
-                .apiKey(aqChatConfig.getAiConfig().getApiKey())
-                .model(aqChatConfig.getAiConfig().getModel())
-                .messages(Arrays.asList(userMsg))
-                .resultFormat(GenerationParam.ResultFormat.MESSAGE)
-                .topP(0.8)
-                .incrementalOutput(true)
-                .build();
     }
 
 }
